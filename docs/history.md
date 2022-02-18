153ac907f4b679dce36d4c58034655ccf93f4039
Michael Myers <michael.morris.myers@gmail.com>
2022-02-18 11:55:34 -0500

    chore:hello world

---
+++ docs/history.md
@@ -0,0 +1,7 @@
+---
+title: History of Pawnee, Indiana
+---
+
+In 1812, Reverend Luther Howell of Terre Haute traveled over 100 miles by pushcart and planted a flag in the ground. That soil, formerly inhabited by the Wamapoke Indian tribe, was to become Pawnee. Rev. Howell was soon dispatched by the tribe, but not before consecrating the town's first church, Pawnee Lutheran, which still stands today, though it was deconsecrated in 1974 and is currently a wholesale linen outlet.
+
+In 2014, the neighboring town of Pawnee, and the county seat of Wamapoke County, Eagleton, was de-incorporated and reabsorbed into Pawnee (citing bankruptcy issues) as part of the Pawnee-Eagleton merger. Subsequently, Pawnee became the county seat of Wamapoke County. The section of Pawnee formerly known as Eagleton is now recognized as the community of “Old Eagleton.”
---
+++ docusaurus.config.js
@@ -0,0 +1,102 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "City of Pawnee, Indiana",
+  tagline: "When you're here, then you're home",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "pawnee-website", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Pawnee, Indiana",
+        logo: {
+          alt: "Seal of Pawnee",
+          src: "img/pawnee-seal.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            type: "doc",
+            docId: "history",
+            position: "left",
+            label: "History",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `CC-BY-SA`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
